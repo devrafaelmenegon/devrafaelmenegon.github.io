28248350715ca10554b618a31a36607a39e6de2e
Rafael Menegon <rafaelmenegon.dev@gmail.com>
2022-01-27 23:58:32 -0300

    :sparkles: feat: navbar bottom

--- navbar.js
+++ navbar.js
@@ -1,18 +1,8 @@
 let burger = document.getElementById('burger'),
-	 nav    = document.getElementById('main-nav');
+	nav    = document.getElementById('main-nav'),
+	body = document.body;
 
 burger.addEventListener('click', function(e){
 	this.classList.toggle('is-open');
 	nav.classList.toggle('is-open');
-});
-
-/* Onload demo - dirty timeout */
-let clickEvent = new Event('click');
-
-window.addEventListener('load', function(e) {
-	burger.dispatchEvent(clickEvent);
-	
-	setTimeout(function(){
-		burger.dispatchEvent(clickEvent);
-	}, 5500);
 });
